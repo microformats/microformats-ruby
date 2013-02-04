07d510ab7de824ac97273b18a8a33fbe15b81ff4
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-02-04 13:40:33 -0800

    allows for parsing of html string, file path, or url

--- lib/microformats2.rb
+++ lib/microformats2.rb
@@ -1,5 +1,16 @@
+require "nokogiri"
+require "open-uri"
 require "microformats2/version"
 
 module Microformats2
-  # Your code goes here...
+  def self.parse(html)
+    html = read_html(html)
+    Nokogiri::HTML(html)
+  end
+
+  def self.read_html(html)
+    open(html).read
+  rescue Errno::ENOENT => e
+    html
+  end
 end
