eaded423d17d34fd665f64b381f1218a380e0154
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-02-13 17:28:08 -0800

    fixes whitespace issue. uses two spaces instead of tabs

--- lib/microformats2/property/embedded.rb
+++ lib/microformats2/property/embedded.rb
@@ -1,9 +1,9 @@
 module Microformats2
-	module Property
+  module Property
     class Embedded < Foundation
       def value
         @value ||= @element.inner_html.strip
       end
     end
-	end
+  end
 end
