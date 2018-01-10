bb74a37bfa2c08035cf52e21bfd99fd44556662d
Ben Roberts <ben@thatmustbe.me>
2018-01-10 09:44:49 -0500

    add failing test

---
+++ spec/support/lib/edge_cases/link_parse.js
@@ -0,0 +1,30 @@
+{
+  "items": [
+    {
+      "type": [
+        "h-entry"
+      ],
+      "properties": {
+        "name": [
+          "Example.com homepage"
+        ],
+        "url": [
+          "https://example.com/" 
+        ]
+      }
+    }
+  ],
+  "rels": {
+    "canonical": [
+      "https://example.com/"
+    ]
+  },
+  "rel-urls": {
+    "https://example.com/": {
+      "title": "Example.com homepage",
+      "rels": [
+        "canonical"
+      ]
+    }
+  }
+}
