beff9b586a2eaf161880642837450d2a08ce8b91
Ben Roberts <ben@thatmustbe.me>
2018-03-29 14:07:08 -0400

    add  test for issue #83

---
+++ spec/support/lib/edge_cases/break_tag.js
@@ -0,0 +1,41 @@
+{
+  "items": [
+    {
+      "type": [
+        "h-card"
+      ],
+      "properties": {
+        "url": [
+          "http://example.org/",
+          "http://flickr.com",
+          "http://twitter.com/jlsuttles"
+        ],
+        "name": [
+          "Jessica Lynn Suttles"
+        ],
+        "bday": [
+          "1990-10-15"
+        ],
+        "content": [
+          {
+            "value": "Vegan.\nCat lover.\nCoder.",
+            "html": "<p>Vegan.<br class=\"auto-break\">Cat lover.<br>Coder.</p>"
+          }
+        ]
+      }
+    }
+  ],
+  "rels": {
+    "me": [
+      "http://twitter.com/jlsuttles"
+    ]
+  },
+  "rel-urls": {
+    "http://twitter.com/jlsuttles": {
+      "text": "@jlsuttles",
+      "rels": [
+        "me"
+      ]
+    }
+  }
+}
